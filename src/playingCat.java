867fa683a96d5c6e27e8a539100228c482d0a81b
CadenceReynolds <reynocal001@warren.k12.in.us>
2024-08-29 12:42:14 -0400

    Finished assignment

--- src/playingCat.java
+++ src/playingCat.java
@@ -1,2 +1,17 @@
-package PACKAGE_NAME;public class playingCat {
+public class playingCat {
+
+        public static void main(String[] args) {
+            System.out.println(isCatPlaying(true, 10));
+            System.out.println(isCatPlaying(false, 36));
+            System.out.println(isCatPlaying(false, 35));
+
+        }
+
+        private static boolean isCatPlaying(boolean isSummer, int temperature) {
+            if(isSummer && (25 <= temperature && temperature <= 35)){
+                return true;
+            }else{
+                return temperature >= 25 && temperature <= 35;
+            }
+        }
 }
